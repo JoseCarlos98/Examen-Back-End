42612c0066c5b9fc7a2289a37cd302df20f2e81b
Jose Carlos <josecarlos231998@gmail.com>
2022-06-16 23:33:15 -0600

    quick fix

--- middlewares/validar-campos.js
+++ middlewares/validar-campos.js
@@ -7,7 +7,7 @@ const validarCampos = (req, res = response, next ) => {
     if (!errores.isEmpty()) {
         return res.status(400).json({
             ok: false,
-            errors: errores.mapped()
+            errors: errores
         });
     }
     next();
